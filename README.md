ed0d4eef39e63682663cb65489c05e1476146003
divarathore <rathd119@newschool.edu>
2021-04-27 01:55:03 +0530

    p5.js background animation & styling

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # numerology
  name numerology calculator
+
+the calculation and information data is from these two sites: 
+https://astrologyfutureeye.com/fortune-tellers/name-numerology-calculator#chart
+https://www.prokerala.com/numerology/destiny-numbers.htm
+
+name numerololgy is a study of no.s assigned to alphabets which have vibrations of energy and expressions.
+this calulator only calculates your Destiny no. which can be calculated by date of birth or birth fullname.
+It basically adds up your alphanumerals which are assigned in the Chaldean Numerology system.
+each no. is added till its a single digit unless its a no. 11 or 22. 
+each no. from 1-9 plus 11 and 22 have an explanation of one's destiny.
+
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+    
 let inputElement = document.querySelector("#input");
 
 let result = document.querySelector("#result");
@@ -9,7 +10,7 @@ inputElement.addEventListener("keydown",function(event){
   // Detect when we specifically press enter key
   if (event.key == "Enter"){
     let total = 0;
-    
+
     
     // Get the contents of the text input (aka name)
     let name = inputElement.value;
@@ -22,7 +23,7 @@ inputElement.addEventListener("keydown",function(event){
     result.innerHTML = "";
     
     // Loop through 4 times
-    for (let i = 0; i < 15; i++){
+    for (let i = 0; i < 50; i++){
       
       // Print the number we're at in the loop
       // console.log(i);
@@ -78,3 +79,72 @@ inputElement.addEventListener("keydown",function(event){
     
   }
 })
+
+
+
+
+
+
+
+
+// this class describes the properties of a single particle.
+class Particle {
+    // setting the co-ordinates, radius and the
+    // speed of a particle in both the co-ordinates axes.
+      constructor(){
+        this.x = random(0,width);
+        this.y = random(0,height);
+        this.r = random(2,8);
+        this.xSpeed = random(-2,2);
+        this.ySpeed = random(-1,1.5);
+      }
+    
+    // creation of a particle.
+      createParticle() {
+        noStroke();
+        fill('rgba(99, 30, 163, 0.76)');
+        circle(this.x,this.y,this.r);
+      }
+    
+    // setting the particle in motion.
+      moveParticle() {
+        if(this.x < 0 || this.x > width)
+          this.xSpeed*=-1;
+        if(this.y < 0 || this.y > height)
+          this.ySpeed*=-1;
+        this.x+=this.xSpeed;
+        this.y+=this.ySpeed;
+      }
+    
+    // this function creates the connections(lines)
+    // between particles which are less than a certain distance apart
+      joinParticles(particles) {
+        particles.forEach(element =>{
+          let dis = dist(this.x,this.y,element.x,element.y);
+          if(dis<85) {
+            stroke('rgba(99, 30, 163, 0.2)');
+            line(this.x,this.y,element.x,element.y);
+          }
+        });
+      }
+    }
+    
+    // an array to add multiple particles
+    let particles = [];
+    
+    function setup() {
+      createCanvas(window.innerWidth, window.innerHeight);
+      for(let i = 0;i<width/10;i++){
+        particles.push(new Particle());
+      }
+    }
+    
+    function draw() {
+      background('#ffffff');
+      for(let i = 0;i<particles.length;i++) {
+        particles[i].createParticle();
+        particles[i].moveParticle();
+        particles[i].joinParticles(particles.slice(i));
+      }
+    }
+
